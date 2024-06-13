f7a8f010b1abe0cf0a6af34f731924826cc10525
devgiljong <calf-love@naver.com>
2024-06-13 20:45:24 +0900

    :construction: Test

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,16 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
