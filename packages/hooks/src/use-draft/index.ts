0c0836eabd0c76282edbcfabf1b0b38ab49e5244
devgiljong <calf-love@naver.com>
2024-06-16 14:46:50 +0900

    :sparkles: Hooks and types

---
+++ packages/hooks/src/use-draft/index.ts
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+type UseDraftProps<T> = {
+  initialState: T;
+  onCommit?: ((state: T) => Promise<void>) | ((state: T) => void);
+};
+/**
+ * 사용자 정의 훅으로, 임시 상태 값을 관리하는 데 사용됩니다.
+ *
+ * @template T - 상태 값의 타입 매개변수입니다.
+ * @param {UseDraftProps<T>} props - 훅에 전달되는 속성 객체입니다.
+ * @returns {{ value: T, onChangeValue: (newValue: T) => void, onCommit: () => Promise<void> | void }} - 훅에서 반환되는 객체입니다.
+ *
+ * @example
+ * const initialState = { name: "", age: 0 };
+ * const onCommitHandler = async (state) => {
+ *   // 상태 값을 서버에 저장하는 비동기 작업 수행
+ *   await saveStateToServer(state);
+ * };
+ *
+ * const { value, onChangeValue, onCommit } = useDraft({ initialState, onCommit: onCommitHandler });
+ *
+ * // 상태 값 변경
+ * onChangeValue({ name: "John", age: 25 });
+ *
+ * // 상태 값 커밋
+ * onCommit();
+ */
+export const useDraft = <T>(props: UseDraftProps<T>) => {
+  const [draft, setDraft] = useState<T>();
+  const value = draft ?? props?.initialState;
+  const onChangeValue = setDraft;
+  const onCommit = () => {
+    return props?.onCommit?.(value);
+  };
+  return { value, onChangeValue, onCommit };
+};
