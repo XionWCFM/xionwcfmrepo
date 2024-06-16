7ce1c3d6b9066b16c6066bad48fc7d8aa4d030ae
devgiljong <calf-love@naver.com>
2024-06-16 14:29:46 +0900

    :construction: Error test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import { add } from "@repo/math/add";
 import { Button } from "@repo/ui/button";
+import { ReactContextError } from "@xionwcfm/error/internal-error";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       {add(1, 2)}
+      <button
+        type="button"
+        onClick={() => {
+          throw new ReactContextError("error", "Home");
+        }}
+      >
+        클릭
+      </button>
       <Button appName="hello">hello</Button>
       <button type="button">sda</button>
     </div>
--- packages/types/polymorphic/index.ts
+++ packages/types/polymorphic/index.ts
@@ -1,8 +1,4 @@
-import type {
-  ComponentPropsWithRef,
-  ComponentPropsWithoutRef,
-  ElementType,
-} from "react";
+import type { ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType } from "react";
 
 export type AsProp<C extends ElementType> = {
   as?: C;
@@ -10,18 +6,11 @@ export type AsProp<C extends ElementType> = {
 
 export type KeyWithAs<C extends ElementType, Props> = keyof (AsProp<C> & Props);
 
-export type PolymorphicRef<C extends ElementType> =
-  ComponentPropsWithRef<C>["ref"];
+export type PolymorphicRef<C extends ElementType> = ComponentPropsWithRef<C>["ref"];
 
-export type PolymorphicComponentProps<
-  C extends ElementType,
-  Props = object,
-> = (Props & AsProp<C>) &
+export type PolymorphicComponentProps<C extends ElementType, Props = object> = (Props & AsProp<C>) &
   Omit<ComponentPropsWithoutRef<C>, KeyWithAs<C, Props>>;
 
-export type PolymorphicComponentPropsWithRef<
-  C extends ElementType,
-  Props = object,
-> = Props & {
+export type PolymorphicComponentPropsWithRef<C extends ElementType, Props = object> = Props & {
   ref?: PolymorphicRef<C>;
 };
