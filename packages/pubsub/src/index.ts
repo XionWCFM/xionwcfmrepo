3443247ac4aadb81ad1768fc78f8d19743e2acbb
devgiljong <calf-love@naver.com>
2024-06-18 16:40:34 +0900

    :construction: Pubsub add

---
+++ packages/pubsub/src/index.ts
@@ -0,0 +1,35 @@
+type DefaultFunction = (...param: any[]) => void;
+export class Pubsub<EventName extends string = string> {
+  private events: Record<string, DefaultFunction[]>;
+  constructor() {
+    this.events = {};
+  }
+  subscribe(eventName: EventName, func: DefaultFunction) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    //@ts-expect-error
+    this.events[eventName].push(func);
+  }
+  unsubscribe(eventName: EventName, func: DefaultFunction) {
+    const handlers = this.events[eventName];
+    if (handlers) {
+      this.events[eventName] = handlers.filter((handler) => handler !== func);
+    }
+  }
+  publish(eventName: EventName, context?: Record<string, any>) {
+    if (!this.events[eventName]) {
+      return;
+    }
+    //@ts-expect-error
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.events[eventName].forEach((func) => func(context));
+  }
+  clear(eventName?: EventName) {
+    if (eventName) {
+      delete this.events[eventName];
+    } else {
+      this.events = {};
+    }
+  }
+}
