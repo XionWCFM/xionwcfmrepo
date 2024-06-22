b48282b315439f9b090d5e8ae4dfc2ff3e46c8b4
devgiljong <calf-love@naver.com>
2024-06-23 01:38:39 +0900

    :construction: Header 구현

--- apps/blog/src/widgets/header/index.tsx
+++ apps/blog/src/widgets/header/index.tsx
@@ -1,12 +1,24 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Image } from "@xionwcfm/adapters/image";
+import { Link } from "@xionwcfm/adapters/link";
 import { Stack } from "@xionwcfm/ui/stack";
 import { ASSET_XION_BOX_LOGO_48_16 } from "~/shared/assets";
+import { ROUTES } from "~/shared/routes";
+
 export const Header = () => {
   return (
-    <Stack as={"header"} direction={"row"} justify={"between"} px={"12"} py={"8"}>
-      <Image {...ASSET_XION_BOX_LOGO_48_16} />
-      <HamburgerMenuIcon />
+    <Stack as={"header"} direction={"row"} align={"center"} justify={"between"} px={"12"} py={"8"}>
+      <Link href={ROUTES.root()}>
+        <Image
+          {...ASSET_XION_BOX_LOGO_48_16}
+          width={ASSET_XION_BOX_LOGO_48_16.width * 2}
+          height={ASSET_XION_BOX_LOGO_48_16.height * 2}
+        />
+      </Link>
+
+      <button type={"button"}>
+        <HamburgerMenuIcon className=" w-24 h-24 text-neutral-400" />
+      </button>
     </Stack>
   );
 };
