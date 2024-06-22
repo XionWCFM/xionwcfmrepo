77ff953759a4cdc8b5c97de11d647d2329e65d81
devgiljong <calf-love@naver.com>
2024-06-22 20:34:15 +0900

    :construction: Constants 수정 및 asset 경로 리졸브

---
+++ apps/blog/src/shared/constants/index.ts
@@ -0,0 +1,29 @@
+// 메타데이터에 대한 상수값입니다.
+export const BASE_SITE_URL = "https://xionwcfmrepo.vercel.app/";
+
+export const BASE_SITE_DESCRIPTION =
+  "글 쓰기를 좋아하는 대한민국의 프론트엔드 개발자입니다. next.js, react, typescript, javascript 등 프론트엔드 개발과 관련된 주제의 글을 작성합니다.";
+
+export const BASE_SITE_NAME = "XionWCFM Frontend Tech Blog";
+
+export const BASE_SITE_TITLE = "XionWCFM의 프론트엔드 기술 블로그";
+
+// author에 대한 상수값입니다.
+export const AUTHOR_NAME = "유길종(XionWCFM)";
+
+export const AUTHOR_DESCRIPTION =
+  "무언가를 쉽게 설명 해낼 때 쾌감을 느끼는 사람입니다.\n현재는 프론트엔드 개발자로 일하고 있습니다.";
+
+export const AUTHOR_EMAIL = "giljongyudev@gmail.com";
+
+export const AUTHOR_COPYRIGHT = "© 2024 YuGilJong. All rights reserved";
+
+export const AUTHOR = {
+  NAME: AUTHOR_NAME,
+  DESCRIPTION: AUTHOR_DESCRIPTION,
+  EMAIL: AUTHOR_EMAIL,
+  COPYRIGHT: AUTHOR_COPYRIGHT,
+};
+
+// 기타 서드파티에 대한 상수값입니다.
+export const LORDICON_LICENSE = "Animated icons by Lordicon.com";
