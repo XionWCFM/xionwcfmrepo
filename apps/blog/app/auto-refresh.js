d761bd486d573355aad70976e75787e66f59a106
devgiljong <calf-love@naver.com>
2024-06-22 16:28:28 +0900

    :sparkles: Icon 설정

---
+++ apps/blog/app/auto-refresh.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+// biome-ignore lint/suspicious/noFunctionAssign: <explanation>
+function AutoRefresh({ children }) {
+  return children;
+}
+
+if (process.env.NODE_ENV === "development") {
+  AutoRefresh = function AutoRefresh({ children }) {
+    const router = useRouter();
+    useEffect(() => {
+      console.log("is work");
+      const ws = new WebSocket("ws://localhost:3001");
+      ws.onmessage = (event) => {
+        if (event.data === "refresh") {
+          router.refresh();
+        }
+      };
+      return () => {
+        ws.close();
+      };
+    }, [router]);
+    return children;
+  };
+}
+
+export default AutoRefresh;
