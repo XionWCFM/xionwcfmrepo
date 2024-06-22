d0213040e509356c3124fb443c67f8c9d3f0d448
devgiljong <calf-love@naver.com>
2024-06-22 20:24:16 +0900

    :sparkles: Asset constants add

---
+++ apps/blog/src/shared/assets/index.ts
@@ -0,0 +1,48 @@
+export const ASSET_XION_CHAR_GRAY = {
+  src: "../../../public/xion-char-gray-60x90.webp",
+  alt: "XionWCFM을 나타내는 캐릭터 이미지",
+  width: "60",
+  height: "90",
+} as const;
+
+export const ASSET_XION_CHAR_WHITE = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM을 나타내는 캐릭터 이미지",
+  width: "135",
+  height: "197",
+} as const;
+
+export const ASSET_XION_BOX_LOGO_48_16 = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM의 로고 이미지",
+  width: "48",
+  height: "16",
+};
+
+export const ASSET_XION_CIRCLE_LOGO_16_16 = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM의 로고 이미지",
+  width: "16",
+  height: "16",
+};
+
+export const ASSET_XION_CIRCLE_LOGO_48_48 = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM의 로고 이미지",
+  width: "48",
+  height: "48",
+};
+
+export const ASSET_XION_SQURE_LOGO_16_16 = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM의 로고 이미지",
+  width: "16",
+  height: "16",
+};
+
+export const ASSET_XION_SQURE_LOGO_48_48 = {
+  src: "../../../public/xion-char-white-60x90.webp",
+  alt: "XionWCFM의 로고 이미지",
+  width: "48",
+  height: "48",
+};
