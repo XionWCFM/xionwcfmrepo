46ce82425fb72b812b5096615d46be7d8d7a53df
devgiljong <calf-love@naver.com>
2024-06-23 18:52:40 +0900

    :construction: Release date 수정

--- apps/blog/src/entities/post/ui/mdx/mdx-components.tsx
+++ apps/blog/src/entities/post/ui/mdx/mdx-components.tsx
@@ -22,21 +22,23 @@ export const MDXComponents: TMDXComponents = {
       {...props}
     />
   ),
-  h1: ({ className, color, ...props }: HProps) => <Paragraph as="h2" color={"neutral-700"} {...props} />,
+  h1: ({ className, color, ...props }: HProps) => (
+    <Paragraph mt="20" mb="12" as="h2" color={"neutral-700"} {...props} />
+  ),
   h2: ({ className, color, ...props }: HProps) => (
-    <Paragraph my="12" as={"h2"} size={"9"} weight={"bold"} color={"neutral-700"} {...props} />
+    <Paragraph mt="20" mb="12" as={"h2"} size={"9"} weight={"bold"} color={"neutral-700"} {...props} />
   ),
   h3: ({ className, color, ...props }: HProps) => (
-    <Paragraph my={"8"} as={"h3"} size={"8"} weight={"bold"} color={"neutral-700"} {...props} />
+    <Paragraph mt="20" mb="12" as={"h3"} size={"8"} weight={"bold"} color={"neutral-700"} {...props} />
   ),
   h4: ({ className, color, ...props }: HProps) => (
-    <Paragraph my="8" as={"h4"} size={"7"} weight={"bold"} color={"neutral-700"} {...props} />
+    <Paragraph mt="20" mb="12" as={"h4"} size={"7"} weight={"bold"} color={"neutral-700"} {...props} />
   ),
   h5: ({ className, color, ...props }: HProps) => (
-    <Paragraph my="8" as={"h5"} size={"6"} weight={"bold"} color={"neutral-700"} {...props} />
+    <Paragraph mt="20" mb="12" as={"h5"} size={"6"} weight={"bold"} color={"neutral-700"} {...props} />
   ),
   h6: ({ className, color, ...props }: HProps) => (
-    <Paragraph my="8" as={"h6"} size={"6"} weight={"bold"} color={"neutral-700"} {...props} />
+    <Paragraph mt="20" mb="12" as={"h6"} size={"6"} weight={"bold"} color={"neutral-700"} {...props} />
   ),
   p: ({ className, color, ...props }: PProps) => (
     <Paragraph size={"5"} mt="24" className=" leading-[240%]" weight={"light"} color={"neutral-600"} {...props} />
@@ -46,4 +48,28 @@ export const MDXComponents: TMDXComponents = {
   ),
   hr: (props) => <hr className=" border-t border-neutral-300 my-24" />,
   code: ({ className, ...props }: CProps) => <code className={" text-size-3"} {...props} />,
+  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+    <ul className={" px-20 py-12 list-disc font-thin text-neutral-600"} {...props} />
+  ),
+  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+    <ol className={" text-neutral-600 font-thin px-20 py-12 list-decimal "} {...props} />
+  ),
+  li: ({ className, ...props }: React.LiHTMLAttributes<HTMLLIElement>) => <li className={" "} {...props} />,
+  table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
+    <div className=" my-6 w-full overflow-y-auto">
+      <table className={"w-full"} {...props} />
+    </div>
+  ),
+  tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
+    <tr className={"m-0 border-t p-0 even:bg-muted"} {...props} />
+  ),
+  th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
+    <th
+      className={"border px-8 py-4 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right"}
+      {...props}
+    />
+  ),
+  td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
+    <td className={"border px-8 py-4 text-left [&[align=center]]:text-center [&[align=right]]:text-right"} {...props} />
+  ),
 };
