9cc44367836782653f218bd441d30e988ea9ef86
devgiljong <calf-love@naver.com>
2024-06-26 18:23:00 +0900

    :bug: Ga, clarity 수정

--- apps/blog/src/app/script/clarity.script.tsx
+++ apps/blog/src/app/script/clarity.script.tsx
@@ -1,26 +1,20 @@
-"use client";
-import { useSearchParams } from "next/navigation";
 import Script from "next/script";
 
 export const ClarityScript = () => {
   return (
-    <>
-      <Script id="clarityScript" type="text/javascript" defer strategy={"afterInteractive"}>{`(function(c,l,a,r,i,t,y){
+    <Script
+      id="clarityScript"
+      type="text/javascript"
+      defer
+      strategy={"afterInteractive"}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+      dangerouslySetInnerHTML={{
+        __html: `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "mvz3tc93tn")`}</Script>
-    </>
+    })(window, document, "clarity", "script", "mvz3tc93tn")`,
+      }}
+    />
   );
-};
-
-export const useHI = () => {
-  const searchParams = useSearchParams();
-  const getAllSearchParams = () => {
-    const obj: Record<string, string> = {};
-    searchParams.forEach((value, key) => {
-      obj[key] = value;
-    });
-    return obj;
-  };
 };
