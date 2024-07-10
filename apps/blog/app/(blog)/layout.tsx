7a94aee03317cea934d9ff29a13e297d4a587633
devgiljong <calf-love@naver.com>
2024-07-11 01:09:57 +0900

    :sparkles: 포스트페이지 1024 지원 제거

--- apps/blog/app/(blog)/layout.tsx
+++ apps/blog/app/(blog)/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <InteractiveHeader />
       <Spacing className=" bg-neutral-50" h={"48"} />
       <Stack className="  overflow-hidden" w={"screen"} align={"center"}>
-        <Stack className="  md:max-w-768 xl:max-w-1024" w={"screen"}>
+        <Stack className="  md:max-w-768 " w={"screen"}>
           {children}
         </Stack>
       </Stack>
