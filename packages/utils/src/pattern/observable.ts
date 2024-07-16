36ddb5669f13ad10f31fe525251d323a3baf4f8e
devgiljong <calf-love@naver.com>
2024-07-16 14:25:46 +0900

    :construction: Pattern

---
+++ packages/utils/src/pattern/observable.ts
@@ -0,0 +1,18 @@
+type Observer<T> = (data: T) => void;
+
+export class Observable<T> {
+  private observers: Observer<T>[] = [];
+
+  subscribe(observer: Observer<T>): void {
+    this.observers.push(observer);
+  }
+
+  unsubscribe(observer: Observer<T>): void {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+
+  notify(data: T): void {
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.observers.forEach((observer) => observer(data));
+  }
+}
