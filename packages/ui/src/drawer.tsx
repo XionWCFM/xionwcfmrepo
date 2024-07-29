184fb583bf1a224704ff4d0293fc7259a9c2f63b
devgiljong <calf-love@naver.com>
2024-07-29 23:33:04 +0900

    :bug: Storybook lint 제거

--- packages/ui/src/drawer.tsx
+++ packages/ui/src/drawer.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import * as React from "react";
+import {
+  type ComponentProps,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  type HTMLAttributes,
+  forwardRef,
+} from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 import { cn } from "./external-utils/cn";
 
-const Root = ({ shouldScaleBackground = true, ...props }: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+const Root = ({ shouldScaleBackground = true, ...props }: ComponentProps<typeof DrawerPrimitive.Root>) => (
   <DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
 );
 Root.displayName = "Drawer";
 
-const DrawerTrigger = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Trigger>
+const DrawerTrigger = forwardRef<
+  ElementRef<typeof DrawerPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Trigger>
 >((props, ref) => {
   const { className, ...rest } = props;
   return <DrawerPrimitive.Trigger {...rest} className={cn(className)} ref={ref} />;
@@ -23,17 +29,17 @@ const DrawerClose = DrawerPrimitive.Close;
 
 const DrawerHandle = DrawerPrimitive.Handle;
 
-const DrawerOverlay = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>
+const DrawerOverlay = forwardRef<
+  ElementRef<typeof DrawerPrimitive.Overlay>,
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay ref={ref} className={`fixed inset-0 z-50 bg-black/80 ${className}`} {...props} />
 ));
 DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 
-const DrawerContent = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
+const DrawerContent = forwardRef<
+  ElementRef<typeof DrawerPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DrawerPortal>
     <DrawerOverlay />
@@ -51,32 +57,33 @@ const DrawerContent = React.forwardRef<
 ));
 DrawerContent.displayName = "DrawerContent";
 
-const DrawerHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+const DrawerHeader = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
   <div className={"grid gap-[6px] p-[16px] text-center sm:text-left"} {...props} />
 );
 DrawerHeader.displayName = "DrawerHeader";
 
-const DrawerFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+const DrawerFooter = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
   <div className={"mt-auto flex flex-col gap-[8px] p-[16px]"} {...props} />
 );
 DrawerFooter.displayName = "DrawerFooter";
 
-const DrawerTitle = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
+const DrawerTitle = forwardRef<
+  ElementRef<typeof DrawerPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Title ref={ref} className={"text-lg font-semibold leading-none tracking-tight"} {...props} />
 ));
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
-const DrawerDescription = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>
+const DrawerDescription = forwardRef<
+  ElementRef<typeof DrawerPrimitive.Description>,
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description ref={ref} className={"text-sm text-muted-foreground"} {...props} />
 ));
 DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
 
+//@ts-nocheck
 export const Drawer = {
   Root,
   Trigger: DrawerTrigger,
