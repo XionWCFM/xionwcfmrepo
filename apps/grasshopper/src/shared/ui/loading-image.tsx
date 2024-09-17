d196a46718821ba6fcb03dc938dcd1dd0f9ace45
devgiljong <calf-love@naver.com>
2024-09-17 13:01:39 +0900

    :construction: Image 처리 추가

--- apps/grasshopper/src/shared/ui/loading-image.tsx
+++ apps/grasshopper/src/shared/ui/loading-image.tsx
@@ -5,20 +5,17 @@ import { cn } from "@xionwcfm/xds";
 import { ComponentProps, Fragment, ReactNode, useState } from "react";
 
 export const LoadingImage = (props: ComponentProps<typeof Image> & { fallback?: ReactNode }) => {
-  const { onLoadStart, onLoad, fallback, className, ...rest } = props;
-  const [loading, setLoading] = useState(false);
-
+  const { onLoad, fallback, className, ...rest } = props;
+  const [isLoaded, setLoaded] = useState(false);
+  const isRenderFallback = !isLoaded;
   return (
     <Fragment>
-      {loading ? fallback : null}
+      {isRenderFallback ? fallback : null}
       <Image
-        onLoadStart={(e) => {
-          onLoadStart?.(e);
-          setLoading(true);
-        }}
-        className={cn(loading && " absolute invisible", className)}
+        className={cn(!isLoaded && " absolute invisible", className)}
         onLoad={(e) => {
-          setLoading(false);
+          setLoaded(true);
+          onLoad?.(e);
         }}
         {...rest}
       />
