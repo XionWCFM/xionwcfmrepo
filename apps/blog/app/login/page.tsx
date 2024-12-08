8dd6460a33c848c7c2ea1e63d904812fe9c82c9d
devgiljong <calf-love@naver.com>
2024-12-08 20:23:24 +0900

    chore: 로그인 폼 구현 progress

--- apps/blog/app/login/page.tsx
+++ apps/blog/app/login/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Stack } from "@xionwcfm/xds";
+import { Button, FixedBottom, FixedBottomCta, Stack } from "@xionwcfm/xds";
+import { JustifyEnd } from "@xionwcfm/xds/justify-end";
+import { toast } from "@xionwcfm/xds/toast";
 import { UnderlineInput } from "@xionwcfm/xds/underline-input";
+import { memo } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
 export default function Page() {
   return (
     <Stack h={"screen"} justify={"center"} items={"center"}>
@@ -14,19 +16,40 @@ export default function Page() {
   );
 }
 
-const LoginForm = () => {
+const LoginForm = memo(() => {
   const form = useForm<FormState>({
     resolver: zodResolver(schema),
   });
 
-  const _handleSubmit = form.handleSubmit((_data) => {});
+  const handleSubmit = form.handleSubmit((data) => {
+    toast.success(data.email);
+  });
 
   return (
     <Stack>
-      <form className="flex min-h-screen flex-col justify-center gap-y-64 w-screen p-32 ">
+      <form onSubmit={handleSubmit} className="flex min-h-screen flex-col justify-center gap-y-64 w-screen p-32 ">
         <UnderlineInput {...form.register("email")} placeholder={"email"} />
         <UnderlineInput {...form.register("password")} placeholder={"password"} />
+        <SubmitButton />
       </form>
+    </Stack>
+  );
+});
+
+const SubmitButton = () => {
+  return (
+    <Stack>
+      <JustifyEnd>
+        <Button className="hidden md:block" type="submit" variant={"emphasis"} size={"lg"}>
+          로그인
+        </Button>
+      </JustifyEnd>
+
+      <FixedBottom>
+        <FixedBottomCta className="md:hidden" type="submit">
+          로그인
+        </FixedBottomCta>
+      </FixedBottom>
     </Stack>
   );
 };
