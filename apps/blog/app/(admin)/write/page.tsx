c88396b94b0f7fc2c47142adf536e188366a520a
devgiljong <calf-love@naver.com>
2024-12-09 02:52:47 +0900

    feat: 익스텐션 추가

--- apps/blog/app/(admin)/write/page.tsx
+++ apps/blog/app/(admin)/write/page.tsx
@@ -5,7 +5,16 @@ import { MdxViewer } from "@repo/mdx";
 export default function Page() {
   return (
     <div>
-      <MdxViewer source="## Hello World **hello**" />
+      <MdxViewer
+        source={`
+## Hello World
+
+\`\`\`js {highlight=1}
+const foo = "bar";
+console.log(foo);
+\`\`\`
+`}
+      />
     </div>
   );
 }
--- packages/mdx/src/MdxViewer.tsx
+++ packages/mdx/src/MdxViewer.tsx
@@ -3,6 +3,10 @@ import { MDXProvider } from "@mdx-js/react";
 import type React from "react";
 import { useEffect, useState } from "react";
 import * as runtime from "react/jsx-runtime";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
 import { MdxComponents } from "./MdxComponents";
 
 export const MdxViewer = ({ source }: { source: string }) => {
@@ -15,6 +19,39 @@ export const MdxViewer = ({ source }: { source: string }) => {
         ...runtime,
         // biome-ignore lint/style/useNamingConvention: <explanation>
         useMDXComponents: () => MdxComponents,
+        remarkPlugins: [remarkBreaks],
+        rehypePlugins: [
+          rehypeSlug,
+          [
+            rehypeAutolinkHeadings,
+            {
+              properties: {
+                className: ["subheading-anchor"],
+                ariaLabel: "Link to section",
+              },
+            },
+          ],
+          [
+            rehypePrettyCode,
+            {
+              theme: "slack-dark",
+              //@ts-expect-error
+              onVisitLine(node) {
+                if (node.children.length === 0) {
+                  node.children = [{ type: "text", value: " " }];
+                }
+              },
+              //@ts-expect-error
+              onVisitHighlightedLine(node) {
+                node.properties.className.push("line--highlighted");
+              },
+              //@ts-expect-error
+              onVisitHighlightedWord(node) {
+                node.properties.className = ["word--highlighted"];
+              },
+            },
+          ],
+        ],
       });
       setContent(() => mdxContent);
     };
