d948cd02641a77d9d5f39ade27ffb6239bdbe5b6
devgiljong <calf-love@naver.com>
2024-12-09 01:50:54 +0900

    chore: login page button 스타일 수정

--- apps/blog/app/login/page.tsx
+++ apps/blog/app/login/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Link } from "@repo/router/link";
-import { Stack } from "@xionwcfm/xds";
+import { Button, Stack } from "@xionwcfm/xds";
 
 export default function Page() {
   return (
     <Stack h={"screen"} justify={"center"} items={"center"}>
-      <Link href={"/api/login/google"} aria-label="클릭">
-        클릭
+      <Link href={"/api/login/google"} aria-label="Google Login">
+        <Button variant={"emphasis"} size={"md"}>
+          Google Login
+        </Button>
       </Link>
     </Stack>
   );
