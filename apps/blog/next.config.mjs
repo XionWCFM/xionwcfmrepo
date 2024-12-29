cb1b3550b59f24126a4a9d417b194a9485300c37
devgiljong <calf-love@naver.com>
2024-12-29 19:23:09 +0900

    feat: tsup config add

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -1,11 +1,7 @@
-import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   transpilePackages: ["next-mdx-remote"],
 };
 
-export default MillionLint.next({
-  enabled: true,
-  rsc: true,
-})(nextConfig);
+export default nextConfig;
---
+++ configs/tailwind/tsup.config.ts
@@ -0,0 +1,4 @@
+import { options } from "@repo/tsup-config";
+import { defineConfig } from "tsup";
+
+export default defineConfig(options);
