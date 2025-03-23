ad7dd1ba3e0e4f3c48b8753c58a08018be52bad8
YuGilJong <101111364+XionWCFM@users.noreply.github.com>
2025-03-24 01:12:05 +0900

    feat: mainpage 개편 (#103)

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -4,50 +4,41 @@ import { getAllPosts } from "~/entities/post/api/getAllPosts";
 import { PostCard } from "~/entities/post/ui/post/PostCard";
 import { AUTHOR_NICKNAME } from "~/shared/constants";
 import { ROUTES } from "~/shared/routes";
-import FadeContent from "~/shared/ui/animations/FadeContent/FadeContent";
-import { Border } from "~/shared/ui/common/Border";
 import { MainTitle } from "~/shared/ui/common/MainTitle";
 import { getFormattedDate } from "~/shared/utils/date/getFormattedDate";
-
 import { Footer } from "~/widgets/footer";
 
 export default async function RootPage() {
   const rawPosts = await getAllPosts();
   const posts = createCurrentPost(rawPosts);
-  const _currentPostTitle = `${AUTHOR_NICKNAME}의 최신 포스트 보기`;
 
   return (
     <>
-      <FadeContent className=" min-h-screen bg-neutral-50 ">
-        <Stack w={"100%"} justify={"center"} items={"center"}>
-          <Stack w={"screen"} maxW={{ md: "768", xl: "1024" }}>
-            <MainTitle />
-            <Stack px={{ initial: "16", md: "0" }}>
-              <Stack>
-                <Border />
-              </Stack>
-              <Stack my={"28"} gap={"16"}>
-                {posts.map((post) => (
-                  <PostCard
-                    key={post.title}
-                    title={post.title}
-                    category={post.category}
-                    description={post.description}
-                    href={ROUTES.postDetail([post.category, post.slug])}
-                    authorNickname={AUTHOR_NICKNAME}
-                    date={getFormattedDate(post.release_date, "yyyy년 MM월 dd일", "0000년 00월 00일")}
-                  />
-                ))}
-              </Stack>
-            </Stack>
+      <MainTitle />
+      <Stack className=" w-full items-center">
+        <Stack className=" max-w-[768px]">
+          <Stack my={"28"} gap={"16"}>
+            {posts.map((post) => (
+              <PostCard key={post.title} {...createPostCardViewModel(post)} />
+            ))}
           </Stack>
+          <Footer />
         </Stack>
-      </FadeContent>
-
-      <Footer />
+      </Stack>
     </>
   );
 }
+
+const createPostCardViewModel = (post: Awaited<ReturnType<typeof getAllPosts>>[number]) => {
+  return {
+    title: post.title,
+    category: post.category,
+    description: post.description,
+    href: ROUTES.postDetail([post.category, post.slug]),
+    authorNickname: AUTHOR_NICKNAME,
+    date: getFormattedDate(post.release_date, "yyyy년 MM월 dd일", "0000년 00월 00일"),
+  };
+};
 
 const createCurrentPost = <T extends { authority?: string; release_date: string }>(posts: T[]) => {
   return posts
--- apps/blog/src/widgets/footer/index.tsx
+++ apps/blog/src/widgets/footer/index.tsx
@@ -2,16 +2,14 @@ import { Paragraph, Stack } from "@xionwcfm/xds";
 import { CONSTANTS } from "~/shared/constants";
 
 export const Footer = () => {
-  const paragraphsList = [`Contact ${CONSTANTS.AUTHOR_EMAIL}`, CONSTANTS.AUTHOR_COPYRIGHT, CONSTANTS.LORDICON_LICENSE];
+  const paragraphsList = [CONSTANTS.AUTHOR_COPYRIGHT, CONSTANTS.LORDICON_LICENSE];
   return (
-    <Stack className=" bg-gray-100" justify={"center"} items={"center"} as={"footer"} py="20" px="12">
-      <Stack>
-        {paragraphsList.map((text) => (
-          <Paragraph leading={"loose"} as={"span"} weight={"thin"} color={"gray-700"} key={text}>
-            {text}
-          </Paragraph>
-        ))}
-      </Stack>
+    <Stack className=" px-16 pb-16">
+      {paragraphsList.map((text) => (
+        <Paragraph leading={"loose"} size={"2"} as={"span"} weight={"thin"} color={"gray-700"} key={text}>
+          {text}
+        </Paragraph>
+      ))}
     </Stack>
   );
 };
