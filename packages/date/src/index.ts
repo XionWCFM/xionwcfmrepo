a4e0114a2ed27224e63462df1370098ae82008fc
XionWCFM <calf-love@naver.com>
2025-05-10 20:26:39 +0900

    feat: bump deps

--- packages/date/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { formatDate } from "./format-date";
-import { isAfterWithSameday } from "./is-after-with-sameday";
-import { isBeforeWithSameday } from "./is-before-with-sameday";
-import { safeGetIso } from "./safe-get-iso";
-import { toDate } from "./to-date";
-import { toIso } from "./to-iso";
-
-export { formatDate, isAfterWithSameday, isBeforeWithSameday, safeGetIso, toDate, toIso };
--- packages/date/src/is-before-with-sameday.ts
+++
@@ -1,6 +0,0 @@
-import { isBefore } from "date-fns/isBefore";
-import { isSameDay } from "date-fns/isSameDay";
-
-export const isBeforeWithSameday = (today: Date, startDate: Date) => {
-  return isBefore(startDate, today) || isSameDay(today, startDate);
-};
