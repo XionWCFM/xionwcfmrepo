0298e0afe56d2f0316696db722354aee8b39855e
XionWCFM <calf-love@naver.com>
2025-05-28 23:25:33 +0900

    chore: 의존성 정리

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["next-mdx-remote"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "https://d2f4hj0sh329cz.cloudfront.net",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
--- packages/router/src/image.tsx
+++ packages/router/src/image.tsx
@@ -1,9 +1,8 @@
 import NextImage from "next/image";
 import { type ComponentProps, type Ref, forwardRef } from "react";
 
-type ImageProps = ComponentProps<typeof NextImage> & { mode?: "noprefix" | "cloudfront" };
+type ImageProps = ComponentProps<typeof NextImage>;
 export const Image = forwardRef(function Image(props: ImageProps, ref: Ref<HTMLImageElement>) {
-  const { src, mode = "noprefix", ...attributes } = props;
-  const imageSrc = props.mode === "cloudfront" ? `https://d2f4hj0sh329cz.cloudfront.net${src}` : props.src;
-  return <NextImage {...attributes} ref={ref} title={attributes.alt} src={imageSrc} />;
+  const { src, ...attributes } = props;
+  return <NextImage {...attributes} ref={ref} title={attributes.alt} src={src} />;
 });
